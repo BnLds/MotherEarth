30012364fa6cb5e107fdd2799721a191c72089fa
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-08-04 16:54:28 +0200

    Add boost animation

--- Scenes/Spaceship/Scripts/spaceship.gd
+++ Scenes/Spaceship/Scripts/spaceship.gd
@@ -4,7 +4,12 @@ class_name Spaceship
 @export var torque_value := 50.0
 @export var boost_value := 100.0
 
+@onready var boost_animation = $BoostAnimation
+
 var has_left_planet:= false
+
+func _ready():
+	PlayBoostAnimation(false)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("rotate_left"):
@@ -18,7 +23,21 @@ func _physics_process(delta):
 		
 		var boost_vect = -transform.y * boost_value
 		apply_force(boost_vect)
+		PlayBoostAnimation(true)
+	else:
+		PlayBoostAnimation(false)
 
 func _on_area_2d_area_entered(area):
 	if area is BigPlanet && has_left_planet:
 		sleeping = true
+		
+
+func PlayBoostAnimation(should_play):
+	if should_play:
+		boost_animation.show()
+		boost_animation.play()
+	else:
+		boost_animation.hide()
+		
+	
+	
