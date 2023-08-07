14ca612560ca196b35696756e0b988c7d3bf6523
Benjamin Landes <90406524+BnLds@users.noreply.github.com>
2023-08-07 14:38:08 +0200

    Center start planet in grid and generate target planet

--- NodelessScripts/PlanetGenerator.gd
+++ NodelessScripts/PlanetGenerator.gd
@@ -1,18 +1,20 @@
-static func GetPointsRandom(n_points, x_width : int, y_width : int, min_dist : int):
+static func GetPointsRandom(n_points, x_width : int, y_width : int, min_dist : int, target_dist : int):
 	var x := []
 	var y := []
-	#var distances := []
+	var target_point
+	var origin = Vector2(floor(x_width/2), floor(y_width/2))
 	
 	var dist_2 := min_dist * min_dist
 	var i_loop := 1
 	var n_valid_points := 0
 	
-	x.append(0)
-	y.append(0)
+	#first planet is origin
+	x.append(origin.x)
+	y.append(origin.y)
 	
 	while n_valid_points < n_points && i_loop < 1e6:
-		var x_new : float = x_width * randf()
-		var y_new : float = y_width * randf()
+		var x_new : int = floor(x_width * randf())
+		var y_new : int = floor(y_width * randf())
 		
 		var is_valid := true
 		for i in range(n_valid_points):
@@ -32,17 +34,16 @@ static func GetPointsRandom(n_points, x_width : int, y_width : int, min_dist : i
 	if n_valid_points < n_points:
 		push_error("Cannot find wanted number of points in " + str(i_loop) + " iterations.")
 	
-#	if n_points > 2:
-#		for i in range(n_points):
-#			for j in range(n_points):
-#				var dx : float = x[i] - x[j]
-#				var dy : float = y[i] - y[j]
-#				var distance := sqrt(dx*dx + dy*dy)
-#				distances[i][j] = distance
-#
-#	else:
-#		distances = [null]
+	if n_points > 2:
+		for i in range(n_points):
+			for j in range(n_points):
+				var dx : float = x[i] - x[j]
+				var dy : float = y[i] - y[j]
+				var distance := sqrt(dx*dx + dy*dy)
+				if distance >= target_dist:
+					target_point = Vector2(x[i], y[i])
+					break
 	
-	return [x, y]
+	return [x, y, origin, target_point]
 
 		
--- Scenes/Level/Scripts/TestGrid.gd
+++ Scenes/Level/Scripts/TestGrid.gd
@@ -3,15 +3,27 @@ extends Control
 @export var bg = Color(1,1,1)
 @export var fg_empty = Color(0.6,0.5,0.5)
 @export var fg_planet = Color(0.6,0.5,0.5)
+@export var fg_start = Color(0.6,0.5,0.5)
+@export var fg_target = Color(0.6,0.5,0.5)
 @export var rows = 100
 @export var columns = 100
+@export var space_size = 200
 
-var rect_size = Vector2(200, 200)
+var rect_size = Vector2(space_size, space_size)
 var PlanetGenerator = preload("res://NodelessScripts/PlanetGenerator.gd")
-var value
+var x
+var y
+var target
+var origin
+var centered_x = []
+var centered_y = []
 
 func _init():
-	value = PlanetGenerator.GetPointsRandom(200, 200, 200, 10)
+	var value = PlanetGenerator.GetPointsRandom(200, space_size, space_size, 10, 50)
+	x = value[0]
+	y = value[1]
+	origin = value[2]
+	target = value[3]
 	
 func _ready():
 	pass
@@ -28,7 +40,12 @@ func _draw():
 		var from := Vector2(0, rect_size.y/rows * i)
 		var to := Vector2(rect_size.x, from.y)
 		draw_line(from, to, fg_empty)
-		
-	for i in range(0, 200):
-		draw_rect(Rect2(Vector2(ceil(value[0][i]), ceil(value[1][i])), Vector2(rect_size.x/columns, rect_size.x/columns)), fg_planet)
-		
+	
+	for i in range(rect_size.x):
+		draw_rect(Rect2(Vector2(x[i], y[i]), Vector2(rect_size.x/columns, rect_size.x/columns)), fg_planet)
+
+	#draw start planet
+	draw_rect(Rect2(origin, Vector2(rect_size.x/columns, rect_size.x/columns)), fg_start)
+	#draw taregt planet
+	draw_rect(Rect2(target, Vector2(rect_size.x/columns, rect_size.x/columns)), fg_target)
+	
